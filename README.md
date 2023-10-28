1b56b9b594b50d2c01a8b433e2980e4060db9ef8
Benny Nazimov <benick12@gmail.com>
2023-10-29 00:47:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Argum Server
+
+## Requirements
+
+- [Rust](https://www.rust-lang.org/tools/install)
+
+## Usage
+
+```bash
+cargo run
+```
+
+## Environment Variables
+
+For a list of environment variables, see the [`.env.example`](.env.example) file.
+
+| Variable | Description       | Default |
+| -------- | ----------------- | ------- |
+| `PORT`   | Port to listen on | `3000`  |
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing::get, Router};
+use dotenvy::dotenv;
+
+async fn hello_world() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
+
+#[tokio::main]
+async fn main() {
+    // env vars are loaded from .env file
+    dotenv().ok();
+    let port = std::env::var("PORT")
+        .expect("PORT env var is not set")
+        .parse::<u16>()
+        .expect("PORT env var is not a valid port");
+
+    // build our application with a route
+    let app = Router::new().route("/", get(hello_world));
+
+    // run it
+    axum::Server::bind(&SocketAddr::from(([0, 0, 0, 0], port)))
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
