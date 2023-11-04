9f5e3cfb5bbd746592bab2c25da250f06578f1f6
Benny Nazimov <66024037+benny-n@users.noreply.github.com>
2023-11-04 18:01:03 +0200

    Websockets POC  :tada:  :tada:  :tada:  :tada:  :tada:  (#10)
    * websocket demo
    
    * Make multiple websockets work
    
    * better logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{
+    extract::{ws::Message, ConnectInfo, State, WebSocketUpgrade},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 use dotenvy::dotenv;
+use futures_util::{SinkExt, StreamExt};
+use tokio::sync::broadcast;
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
-use tracing::info;
+use tracing::{error, info};
 
 use crate::api::{get_room, get_rooms};
 
@@ -11,6 +22,70 @@ mod api;
 mod db;
 mod message;
 mod room;
+
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    State(state): State<Arc<AppState>>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    info!("Got a WebSocket connection from: {}", addr);
+    ws.on_failed_upgrade(move |error| {
+        error!("Failed to upgrade WebSocket connection: {:?}", error);
+    })
+    .on_upgrade(move |socket| async move {
+        info!("New WebSocket connection: {}", addr);
+        let (mut sender, mut receiver) = socket.split();
+
+        let tx = state.tx.lock().unwrap().clone();
+
+        // Subscribe before sending joined message.
+        let mut rx = tx.subscribe();
+
+        // Send joined message to all subscribers.
+        let msg = format!("{} joined.", addr);
+        info!("{}", msg);
+        // let _ = tx.send(msg);
+
+        // This task will receive broadcast messages and send text message to our client.
+        let mut send_task = tokio::spawn(async move {
+            while let Ok(msg) = rx.recv().await {
+                // In any websocket error, break loop.
+                if sender.send(Message::Text(msg)).await.is_err() {
+                    break;
+                }
+            }
+        });
+
+        // This task will receive messages from client and send them to broadcast subscribers.
+        let mut recv_task = tokio::spawn(async move {
+            while let Some(Ok(Message::Text(text))) = receiver.next().await {
+                // Add username before message.
+                let msg = format!("{}: {}", addr, text);
+                info!("Sending message: {}", msg);
+                let _ = tx.send(msg);
+            }
+        });
+
+        // If any one of the tasks exit, abort the other.
+        tokio::select! {
+            _ = (&mut send_task) => recv_task.abort(),
+            _ = (&mut recv_task) => send_task.abort(),
+        };
+        println!("WebSocket closed: {}", addr);
+    })
+}
+
+struct AppState {
+    tx: Mutex<broadcast::Sender<String>>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            tx: Mutex::new(broadcast::channel(10).0),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -23,7 +98,7 @@ async fn main() {
 
     // enable logging
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         .init();
 
     info!("Starting server on port {}", port);
@@ -31,17 +106,23 @@ async fn main() {
     // cors
     let cors = CorsLayer::permissive();
 
+    // state
+    let state = Arc::new(AppState::new());
+
     // build our application with a route
     let app = Router::new()
         .route("/healthcheck", get(|| async { StatusCode::OK }))
         .route("/rooms", get(get_rooms))
         .route("/room/:room_id", get(get_room))
+        .route("/ws", get(ws_handler))
+        .with_state(state)
         .layer(cors)
         .layer(TraceLayer::new_for_http());
 
     // run it
     axum::Server::bind(&SocketAddr::from(([0, 0, 0, 0], port)))
-        .serve(app.into_make_service())
+        // .serve(app.into_make_service())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
 }
