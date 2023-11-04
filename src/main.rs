77fc0ad6a37c3324cdf4bd616b1b60c0d7a03a00
Benny Nazimov <66024037+benny-n@users.noreply.github.com>
2023-11-04 12:48:36 +0200

    Add cors (#9)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::net::SocketAddr;
 
 use axum::{http::StatusCode, routing::get, Router};
 use dotenvy::dotenv;
-use tower_http::trace::TraceLayer;
+use tower_http::{cors::CorsLayer, trace::TraceLayer};
 use tracing::info;
 
 use crate::api::{get_room, get_rooms};
@@ -28,11 +28,15 @@ async fn main() {
 
     info!("Starting server on port {}", port);
 
+    // cors
+    let cors = CorsLayer::permissive();
+
     // build our application with a route
     let app = Router::new()
         .route("/healthcheck", get(|| async { StatusCode::OK }))
         .route("/rooms", get(get_rooms))
         .route("/room/:room_id", get(get_room))
+        .layer(cors)
         .layer(TraceLayer::new_for_http());
 
     // run it
